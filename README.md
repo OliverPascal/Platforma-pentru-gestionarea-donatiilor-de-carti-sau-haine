ac6f1ce5b46198cd2f9577f9372e330c1c1a7d56
OliverPascal <oliver-andrei.pascal@student.upt.ro>
2026-06-04 12:47:07 +0000

    Am adaugat fisierele proiectului

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Platforma-pentru-gestionarea-donatiilor-de-carti-sau-haine
+#  Platformă pentru Gestionarea Donațiilor
+
+Aceasta este o aplicație web dezvoltată în **Streamlit** care ajută la corelarea donațiilor cu nevoile ONG-urilor.
+
+##  Echipa noastră 
+ **Membru 1 (Nume Prenume)** - Modulul 1: Autentificare și gestionare utilizatori (`auth.py`)
+ **Membru 2 (Livadaru Raluca-Paula)** - Modulul 2: Interfață Donatori (`donator.py`)
+ **Membru 3 (Pascal Oliver-Andrei)** - Modulul 3: Interfață ONG / Cereri (`ong.py`)
+ **Membru 4 (Nume Prenume)** - Modulul 4: Motor de Matching și Rapoarte (`rapoarte.py`)
+
+
---
+++ ong.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+
+FISIER_CERERI = 'cereri.csv'
+
+def afiseaza_ong():
+    st.header("Catalog si Cereri")
+    
+    if st.session_state["logat"] and st.session_state["rol"] == "ONG / Beneficiar":
+        with st.form("form_cerere", clear_on_submit=True):
+            st.subheader("Ce aveti nevoie?")
+            cat_cerere = st.selectbox("Categorie cautata", ["Carti", "Haine", "Jucarii"])
+            desc_cerere = st.text_input("Descrieti necesarul (ex: 20 de perechi de ghete marimea 40)")
+            submit_cerere = st.form_submit_button("Lanseaza Cererea")
+            
+            if submit_cerere and desc_cerere:
+                df_cereri = pd.read_csv(FISIER_CERERI)
+                noul_id = len(df_cereri) + 1
+                noua_cerere = pd.DataFrame([{
+                    'id': noul_id, 'beneficiar': st.session_state["username"], 
+                    'categorie': cat_cerere, 'descriere': desc_cerere, 'status': 'In asteptare'
+                }])
+                df_cereri = pd.concat([df_cereri, noua_cerere], ignore_index=True)
+                df_cereri.to_csv(FISIER_CERERI, index=False)
+                st.success("Cererea a fost inregistrata!")
+
+        st.markdown("---")
+        st.subheader("Cererile tale active")
+        df_cereri = pd.read_csv(FISIER_CERERI)
+        cererile_mele = df_cereri[df_cereri['beneficiar'] == st.session_state['username']]
+        st.dataframe(cererile_mele[['categorie', 'descriere', 'status']], use_container_width=True)
+    else:
+        st.warning("Trebuie sa fii logat ca ONG / Beneficiar pentru a accesa aceasta pagina.")
