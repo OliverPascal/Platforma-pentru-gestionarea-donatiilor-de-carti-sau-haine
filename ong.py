448cfcd879b6ead3012ce6ce9458562c905292c4
OliverPascal <oliver-andrei.pascal@student.upt.ro>
2026-06-09 17:13:21 +0300

    Am adugat comentarii pentru o mai usoara intelegere
    Added comments for clarity and improved code documentation.

--- ong.py
+++ ong.py
@@ -1,18 +1,22 @@
-import streamlit as st
-import pandas as pd
+import streamlit as st #Biblioteca Streamlit este folosita pentru a crea butoane, formulare si tabele direct in Python
+import pandas as pd    #Biblioteca Pandas este esentiala pentru citirea, modificarea si salvarea tabelelor de date
 
-FISIER_CERERI = 'cereri.csv'
+FISIER_CERERI = 'cereri.csv'  #O constanta care retine numele fisierului CSV  unde stocam toate cererile ONG-urilor
 
 def afiseaza_ong():
     st.header("Catalog si Cereri")
-    
+
+    #Norma de securitate:
     if st.session_state["logat"] and st.session_state["rol"] == "ONG / Beneficiar":
+
+        #Grupeaza elementele intr-un formular
         with st.form("form_cerere", clear_on_submit=True):
             st.subheader("Ce aveti nevoie?")
             cat_cerere = st.selectbox("Categorie cautata", ["Carti", "Haine", "Jucarii"])
             desc_cerere = st.text_input("Descrieti necesarul (ex: 20 de perechi de ghete marimea 40)")
             submit_cerere = st.form_submit_button("Lanseaza Cererea")
-            
+
+            #Salvarea efectiva a datelor
             if submit_cerere and desc_cerere:
                 df_cereri = pd.read_csv(FISIER_CERERI)
                 noul_id = len(df_cereri) + 1
@@ -24,6 +28,7 @@ def afiseaza_ong():
                 df_cereri.to_csv(FISIER_CERERI, index=False)
                 st.success("Cererea a fost inregistrata!")
 
+        #Structura pentru noua cerere
         st.markdown("---")
         st.subheader("Cererile tale active")
         df_cereri = pd.read_csv(FISIER_CERERI)
